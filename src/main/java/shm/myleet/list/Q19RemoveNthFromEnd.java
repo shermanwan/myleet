0993078acd1ebf5470f9107f6546ce86bdb5e598
z003wkkn <xiaomin.wan@siemens.com>
2021-04-26 10:39:09 +0800

    feat: q19 complete

--- src/main/java/shm/myleet/list/Q19RemoveNthFromEnd.java
+++ src/main/java/shm/myleet/list/Q19RemoveNthFromEnd.java
@@ -6,7 +6,31 @@ import shm.myleet.common.ListNode;
 class Q19RemoveNthFromEnd {
 
   public ListNode removeNthFromEnd(ListNode head, int n) {
+    // 虚拟节点头
+    ListNode virtualHead = new ListNode();
+    virtualHead.next = head;
 
+    ListNode fast = virtualHead, slow = virtualHead;
+    int i = 0;
+    while (i < n) {
+      // 输入连表长度不满 n
+      if (fast.next == null) {
+        return virtualHead.next;
+      }
+      fast = fast.next;
+      i++;
+    }
+
+    while (fast.next != null) {
+      slow = slow.next;
+      fast = fast.next;
+    }
+
+    if (slow.next != null) {
+      slow.next = slow.next.next;
+    }
+
+    return virtualHead.next;
   }
 
 }
--- src/test/java/shm/myleet/list/Q19RemoveNthFromEndTests.java
+++ src/test/java/shm/myleet/list/Q19RemoveNthFromEndTests.java
@@ -1,5 +1,33 @@
 package shm.myleet.list;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import shm.myleet.common.ListNode;
+
 public class Q19RemoveNthFromEndTests {
+
+  Q19RemoveNthFromEnd solution = new Q19RemoveNthFromEnd();
+
+  @Test
+  public void basic1() {
+    ListNode input = ListNode.valueOf(new int[] { 1, 2, 3, 4, 5 });
+    ListNode result = solution.removeNthFromEnd(input, 2);
+    Assertions.assertArrayEquals(new int[] { 1, 2, 3, 5 }, ListNode.toArray(result));
+  }
+
+  @Test
+  public void basic2() {
+    ListNode input = ListNode.valueOf(new int[] { 1 });
+    ListNode result = solution.removeNthFromEnd(input, 1);
+    Assertions.assertArrayEquals(new int[] {}, ListNode.toArray(result));
+  }
+
+  @Test
+  public void basic3() {
+    ListNode input = ListNode.valueOf(new int[] { 1, 2 });
+    ListNode result = solution.removeNthFromEnd(input, 1);
+    Assertions.assertArrayEquals(new int[] { 1 }, ListNode.toArray(result));
+  }
 
 }
