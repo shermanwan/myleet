8b2ef85454f9fbe265bb40647cbd155c3835ba7f
shermanwan <shermanwan@163.com>
2021-05-13 07:11:51 +0800

    feat: isValid

---
+++ src/main/java/shm/myleet/stack/Q20IsValid.java
@@ -0,0 +1,24 @@
+package shm.myleet.stack;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 20. 有效的括号
+ *
+ * @see https://leetcode-cn.com/problems/valid-parentheses/
+ */
+@Slf4j
+public class Q20IsValid {
+
+  /**
+   * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。 有效字符串需满足：
+   * 左括号必须用相同类型的右括号闭合。 左括号必须以正确的顺序闭合。
+   *
+   * @param s
+   * @return
+   */
+  public boolean isValid(String s) {
+
+  }
+
+}
---
+++ src/test/java/shm/myleet/stack/Q20IsValidTests.java
@@ -0,0 +1,19 @@
+package shm.myleet.stack;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Q20IsValidTests {
+
+  Q20IsValid solution = new Q20IsValid();
+
+  @Test
+  public void basic1() {
+    Assertions.assertEquals(true, solution.isValid("()"));
+    Assertions.assertEquals(true, solution.isValid("()[]{}"));
+    Assertions.assertEquals(false, solution.isValid("(]"));
+    Assertions.assertEquals(false, solution.isValid("([)]"));
+    Assertions.assertEquals(true, solution.isValid("{[]}"));
+  }
+
+}
