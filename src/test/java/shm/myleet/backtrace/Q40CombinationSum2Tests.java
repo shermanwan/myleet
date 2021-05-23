f39bbd565661700a27268ecbbda46cb4ccae82c8
shermanwan <shermanwan@163.com>
2021-05-24 07:44:04 +0800

    feat: Q40CombinationSum2

--- src/test/java/shm/myleet/backtrace/Q40CombinationSum2Tests.java
+++ src/test/java/shm/myleet/backtrace/Q40CombinationSum2Tests.java
@@ -3,7 +3,6 @@ package shm.myleet.backtrace;
 import java.util.List;
 
 import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
 
 import shm.myleet.common.AssertExt;
 
@@ -15,7 +14,31 @@ public class Q40CombinationSum2Tests {
   public void demo1() {
     int[] input = { 10, 1, 2, 7, 6, 1, 5 };
     List<List<Integer>> actual = solution.combinationSum2(input, 8);
-    int[][] expect = { { 1, 7 }, { 1, 2, 5 }, { 2, 6 }, { 1, 1, 6 } };
+    int[][] expect = { { 1, 1, 6 }, { 1, 2, 5 }, { 1, 7 }, { 2, 6 }, };
+    AssertExt.assertArraysEqual(expect, AssertExt.valueOf(actual));
+  }
+
+  @Test
+  public void demo2() {
+    int[] input = { 2, 5, 2, 1, 2 };
+    List<List<Integer>> actual = solution.combinationSum2(input, 5);
+    int[][] expect = { { 1, 2, 2 }, { 5 } };
+    AssertExt.assertArraysEqual(expect, AssertExt.valueOf(actual));
+  }
+
+  @Test
+  public void boundary1() {
+    int[] input = { 1, 3 };
+    List<List<Integer>> actual = solution.combinationSum2(input, 5);
+    int[][] expect = {};
+    AssertExt.assertArraysEqual(expect, AssertExt.valueOf(actual));
+  }
+
+  @Test
+  public void boundary2() {
+    int[] input = {};
+    List<List<Integer>> actual = solution.combinationSum2(input, 5);
+    int[][] expect = {};
     AssertExt.assertArraysEqual(expect, AssertExt.valueOf(actual));
   }
 
